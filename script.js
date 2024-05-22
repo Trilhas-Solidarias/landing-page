bf88f058e6b737219ee1f828cb6cdb3234de6562
Naum Leal <naumlldiniz@gmail.com>
2024-05-22 00:25:51 -0300

    first commit

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ function show() {
         senha.type = 'password'
     }
 }
+
+document.getElementById('projetos').addEventListener('click', function () {
+    this.classList.add('clicked')
+})
+
+// var nome = 'Naum'
+// var sobreNome = 'Leal'
+// var atraso = 200
+// var textoElemento = document.querySelector('.apresentacao__conteudo__titulo')
+// var textoColorido = document.querySelector('.titulo-destaque')
+
+// var contadorNome = 0
+// var contadorSobreNome = 0
+
+// function escreverNome() {
+//     if (contadorNome < nome.length) {
+//         textoElemento.textContent += nome.charAt(contadorNome)
+//         console.log('Escrevendo nome:', nome.charAt(contadorNome))
+//         contadorNome++
+//         setTimeout(escreverNome, atraso)
+//     } else {
+//         console.log('Nome completo escrito. Iniciando sobrenome...')
+//         escreverSobreNome()
+//     }
+// }
+
+// function escreverSobreNome() {
+//     if (contadorSobreNome < sobreNome.length) {
+//         textoColorido.textContent += sobreNome.charAt(contadorSobreNome)
+//         console.log(
+//             'Escrevendo sobrenome:',
+//             sobreNome.charAt(contadorSobreNome)
+//         )
+//         contadorSobreNome++
+//         setTimeout(escreverSobreNome, atraso)
+//     }
+// }
+
+// // Inicia o processo escrevendo o nome primeiro
+// escreverNome()
