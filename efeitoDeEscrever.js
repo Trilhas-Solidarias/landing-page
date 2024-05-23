d6776645fe6a2f6931de599147b4a1deb703d506
Naum Leal <naumlldiniz@gmail.com>
2024-05-23 19:10:57 -0300

    efeito digitação

---
+++ efeitoDeEscrever.js
@@ -0,0 +1,34 @@
+var texto = 'NAUM '
+var texto2 = 'LEAL'
+
+var atraso = 200
+var textoElemento = document.querySelector('.apresentacao__conteudo__titulo')
+
+var strongElement = document.createElement('strong') // Criar um novo elemento strong
+strongElement.classList.add('titulo-destaque') // Definir a classe do novo strong
+
+var contadorDeLetras = 0
+function escreverTexto() {
+    if (contadorDeLetras < texto.length) {
+        textoElemento.textContent += texto.charAt(contadorDeLetras)
+        contadorDeLetras++
+        setTimeout(escreverTexto, atraso)
+    } else {
+        escrever() // Chamar a função escrever() após escrever o texto "NAUM"
+    }
+}
+
+function escrever() {
+    var contadorDeLetras2 = 0 // Reiniciar o contador
+    function escrever2() {
+        if (contadorDeLetras2 < texto2.length) {
+            strongElement.textContent += texto2.charAt(contadorDeLetras2)
+            contadorDeLetras2++
+            setTimeout(escrever2, atraso)
+        }
+    }
+    escrever2() // Iniciar a função escrever2()
+    textoElemento.appendChild(strongElement) // Adicionar o novo strong como filho do h1
+}
+
+escreverTexto()
